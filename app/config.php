bc0eff4983e604bf0226f075f51dd7dd19dced94
jeisson <jibarguen@suyo.co>
2020-02-28 10:55:25 -0500

    config hora

--- app/config.php
+++ app/config.php
@@ -2,6 +2,7 @@
 //include 'librerias/sesion.php';
 include 'librerias/conexion.php';
 $fecha_registro = date('Y-m-d H:mm:ss');
+$hora = date('H:mm:ss');
 //$serv_arch = "https://sicmath.000webhostapp.com";
 @$serv_arch = $_SERVER['REMOTE_HOST']."/app";
 //include 'modelos/funciones.php'; // Funcionares generales.
